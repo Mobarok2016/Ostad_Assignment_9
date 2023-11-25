d8a78395d65dcaf8413f4f4767c9e279f7a1e58d
unknown <mobarokhossain448@gmail.com>
2023-11-25 20:06:29 +0600

    redirect updated

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProjectController;
 
+Route::get('/',function(){
+    return redirect('/portfolio/home');
+});
+
 Route::group(['prefix'=>'portfolio'],function(){
     Route::get('/home',[HomeController::class,'home'])->name('home');
     Route::get('/about',[AboutController::class,'about'])->name('about');
